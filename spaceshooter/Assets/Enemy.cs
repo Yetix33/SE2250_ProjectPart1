f0e4b72494ba8b13694316c8dc9d92a9c75fc9c2
Yetix33 <apate388@uwo.ca>
2018-02-22 18:29:38 -0500

    First Edit
    Enemy deletion not done

---
+++ spaceshooter/Assets/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+	public float speed = 10f;
+	BoundsCheck idk;
+
+	void Awake(){
+		idk = GetComponent<BoundsCheck> ();
+	}
+
+	public Vector3 pos{
+		get{ 
+			return(this.transform.position);
+		}
+		set{ 
+			this.transform.position = value;
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Move();
+		if (!(idk.checkenemy())) {
+			print("yikes");
+			//gameObject.SetActive (false);
+		}
+			
+	}
+	public virtual void Move(){
+		//idfkwtfdywfm
+	}
+}
--- spaceshooter/Assets/Hero.cs
+++ spaceshooter/Assets/Hero.cs
@@ -8,10 +8,7 @@ public class Hero : MonoBehaviour {
 	public float rollMult = 45;
 	public float pitchMult = 15;
 	//public float shieldLevel = 1;
-	public float radius = 3f;
-	public float camWidth;
-	public float camHeight;
-
+	 BoundsCheck idk;
 
 
 	// Use this for initialization
@@ -20,11 +17,8 @@ public class Hero : MonoBehaviour {
 			S = this;
 		} else {
 			Debug.LogError("Hero.Awake() - attempt to assing two hero" );
-		
-		
 		}
-		camHeight = Camera.main.orthographicSize;
-		camWidth = camHeight * Camera.main.aspect;
+		idk = GetComponent<BoundsCheck>();
 	}
 
 	// Update is called once per frame
@@ -41,29 +35,7 @@ public class Hero : MonoBehaviour {
 
 	}
 	void LateUpdate(){
-	
-		Vector3 pos = transform.position;
-		if (pos.x > camWidth - radius) {
-			pos.x = camWidth - radius;
-		}
-		if (pos.x < -camWidth + radius) {
-			pos.x = -camWidth + radius;
-		}
-		if (pos.y > camHeight - radius) {
-			pos.y = camHeight - radius;
-		}
-		if (pos.y < -camHeight + radius) {
-			pos.y = -camHeight + radius;
-	}
-		transform.position = pos;
-	}
+		idk.check();
 
-	/*void OnDrawGizmos(){
-		if (!Application.isPlaying)
-			return;
-		Vector2 bound = new Vector2 (camWidth * 2, camHeight * 2);
-		Gizmos.DrawWireCube (Vector2.zero, bound);
-	
-	}*/
-
+}
 }
