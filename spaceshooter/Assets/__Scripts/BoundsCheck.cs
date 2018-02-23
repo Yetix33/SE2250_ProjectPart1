aadc066dae92fe45ad4025321ceabe1823f108b0
Sanjot Boyal <sboyal2@uwo.ca>
2018-02-23 04:43:23 -0500

    Phase 1- Part 1
    Edited and fixed- Part 1

---
+++ spaceshooter/Assets/__Scripts/BoundsCheck.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundsCheck : MonoBehaviour {
+	public float radius = 1f;
+	public float camWidth;
+	public float camHeight;
+
+	void Awake(){
+		camHeight = Camera.main.orthographicSize;
+		camWidth = camHeight * Camera.main.aspect;
+	}
+
+
+
+	void OnDrawGizmos(){
+		if (!Application.isPlaying)
+			return;
+		Vector2 bound = new Vector2 (camWidth * 2, camHeight * 2);
+		Gizmos.DrawWireCube (Vector2.zero, bound);
+	}
+
+
+
+
+	void LateUpdate(){
+		Vector3 pos = transform.position;
+
+		if (pos.x > camWidth - radius) {
+			pos.x = camWidth - radius;
+		}
+
+		if (pos.x < -camWidth + radius) {
+			pos.x = -camWidth + radius;
+		}
+
+		if (pos.y > camHeight - radius) {
+			pos.y = camHeight - radius;
+		}
+
+	if (pos.y < -camHeight + radius) {
+			pos.y = -camHeight + radius;
+		}
+		
+		transform.position = pos;
+	}
+}
+
